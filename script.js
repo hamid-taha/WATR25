b94426069c4e50b981f57747840c5e0ae0f0e8de
Abd-Elhamid M. Taha <143228078+hamid-taha@users.noreply.github.com>
2025-03-17 15:31:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.querySelector('.menu-icon');
+  const navList = document.querySelector('.nav-list');
+
+  menuIcon.addEventListener('click', function () {
+    navList.classList.toggle('active');
+  });
+
+  // Smooth scrolling for navigation links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+      navList.classList.remove('active');
     });
   });
 });
